371b58ea4390002ec884f32cad461eee7828787e
Bellamy Smith <bellamysmith1@gmail.com>
2018-07-13 12:04:29 -0700

    adding apn_to_geom api script

---
+++ ruby/apn_to_geom.rb
@@ -0,0 +1,37 @@
+# This was written to simplify a workflow of manually matching APN data in a csv. This uses the LA city API to get geom map data by APN and ads the data to a csv.
+
+require 'csv'
+require 'httparty'
+require 'json'
+
+url = ENV['csv_url']
+csv = CSV.read(url, :headers=>true)
+
+def get_geom_data_from_api(apn) do
+  api_url = "https://data.lacity.org/resource/sa2q-dxtv.json?bpp=#{apn}"
+  response = HTTParty.get(api_url)
+  body = JSON.parse(response.body)
+  if body.length > 0
+    geom = body[0]['the_geom']
+  else
+    geom = ''
+  end
+  # geom string.....
+  gstring = geom.to_s
+  return gstring
+end
+
+# Loop through each row in the CSV
+csv.each do |row|
+  apn = row["APN"].gsub('-','')
+  geom = get_geom_data_from_api(apn)
+  g = geom.gsub('\"', "'")
+  # write to new "geom" header for each row, and change from ruby to json. The JSON parser does not like the geom response as a hash.
+  row["geom"] = g.gsub('=>', ':')
+end
+
+# write back to original csv file
+CSV.open(url, "w") do |f|
+  f << csv.headers
+  csv.each{|row| f << row}
+end
